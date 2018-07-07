414223e097469f14a4216a49fa304ddc2ce952c4
KendrickPC <kennethpchang@gmail.com>
2018-07-07 18:51:18 +0800

    made the alien ship appear on the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,7 +32,6 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
         # Creating an instance of alien
         gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
--- game_functions.py
+++ game_functions.py
@@ -45,7 +45,7 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """ Update images on the screen and flip to a new screen. """
     # Redraw screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -54,6 +54,7 @@ def update_screen(ai_settings, screen, ship, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
